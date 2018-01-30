43e8b2b9877f273be7229b3485372a07e916994e
gabemontero <gmontero@redhat.com>
2018-01-30 16:40:58 -0500

    [maven-release-plugin] prepare for next development iteration

--- src/main/java/io/fabric8/jenkins/openshiftsync/ConfigMapWatcher.java
+++ src/main/java/io/fabric8/jenkins/openshiftsync/ConfigMapWatcher.java
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
  * Copyright (C) 2017 Red Hat, Inc.
-=======
- * Copyright (C) 2016 Red Hat, Inc.
->>>>>>> 129b1fd... support syncing ConfigMap's with config.xml files <-> Jenkins Jobs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
